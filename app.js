ab8d5779b04c6891c8abd88ff625ceeb8eee508e
pnadalini <pietro.nadalini@hotmail.com>
2018-10-25 21:01:47 -0600

    Added cors and Page not found validation

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/robot.route');
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/api/v1/robots', indexRouter);
 
@@ -29,6 +31,11 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
+  if (req.accepts('json')) {
+    res.send({ error: 'Page not found' });
+    return;
+  }
+  res.render('error');
 });
 
 module.exports = app;
